5892c72198970a4b5849a08edc59f9325ecda1f6
MehNameIsJeff <53691495+MehNameIsJeff@users.noreply.github.com>
2019-08-04 17:39:03 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,20 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '~prefix') {
+    	message.reply('My Prefix is '~' ');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '~info') {
+    	message.reply('This bot is made by CanWeabou#6284');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '~help') {
+    	message.reply('Commands for this bot is : ~info, ~prefix');
   	}
 });
 
