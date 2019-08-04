de57bb8e7e08f898d93d01cf65a564ad7e9966c9
MehNameIsJeff <53691495+MehNameIsJeff@users.noreply.github.com>
2019-08-04 17:23:06 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
-cosnt Discord = require('discord.js'):
-cosnt client = new Discord.Client();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am Ready Sir!')
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-   if (message.content === 'ping') {
-     message.reply('pong');
-   }
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-// THIS MUST BE THIS WAY
-client.login(process.env.yhCqA7I4GRilrblA8iSJ3MeitiSzhoUp);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
